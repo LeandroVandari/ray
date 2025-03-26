d2715a03809c1b4566b65ea0e10e9d76db8e1768
LeandroVandari <bernardokreutz@gmail.com>
2025-03-25 23:38:49 -0300

    Created surface configuration.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,7 @@ use std::sync::Arc;
 
 use pollster::FutureExt as _;
 use wgpu::{
-    Backends, CreateSurfaceError, Device, DeviceDescriptor, Instance, InstanceDescriptor, Queue,
-    RequestAdapterOptions, Surface,
+    Adapter, Backends, CreateSurfaceError, Device, DeviceDescriptor, Instance, InstanceDescriptor, Queue, RequestAdapterOptions, Surface, SurfaceConfiguration,TextureUsages
 };
 use winit::{
     application::ApplicationHandler,
@@ -21,24 +20,15 @@ struct SurfaceState<'window> {
     surface: Surface<'window>,
     device: Device,
     queue: Queue,
+    config: SurfaceConfiguration
 }
 
 impl<'window> SurfaceState<'window> {
     fn new(event_loop: &winit::event_loop::ActiveEventLoop) -> Result<Self, CreateSurfaceError> {
-        let instance_desc = InstanceDescriptor {
-            backends: Backends::all(),
-            ..Default::default()
-        };
-        let instance = Instance::new(&instance_desc);
+        let instance = Self::create_instance();        
 
         let window = Arc::new(
-            event_loop
-                .create_window(
-                    WindowAttributes::default()
-                        .with_maximized(true)
-                        .with_title("Ray tracer"),
-                )
-                .unwrap(),
+            Self::create_window(event_loop)
         );
 
         let surface = instance.create_surface(window.clone())?;
@@ -50,17 +40,48 @@ impl<'window> SurfaceState<'window> {
             })
             .block_on()
             .unwrap();
+
+
         let (device, queue) = adapter
             .request_device(&DeviceDescriptor::default(), None)
             .block_on()
             .unwrap();
+
+        let config = Self::create_surface_configuration(&surface, &adapter, &window);
 
         Ok(Self {
             surface,
             window,
             device,
             queue,
+            config
         })
+    }
+
+    fn create_instance() -> Instance {
+        let instance_desc = InstanceDescriptor {
+            backends: Backends::all(),
+            ..Default::default()
+        };
+        Instance::new(&instance_desc)
+    }
+
+    fn create_window(event_loop: &winit::event_loop::ActiveEventLoop) -> Window {
+        event_loop
+                .create_window(
+                    WindowAttributes::default()
+                        .with_maximized(true).with_resizable(false)
+                        .with_title("Ray tracer"),
+                )
+                .unwrap()
+    }
+
+    fn create_surface_configuration(surface: &Surface, adapter: &Adapter, window: &Window) -> SurfaceConfiguration {
+        let surface_caps = surface.get_capabilities(adapter);
+
+        let surface_format = surface_caps.formats.iter().find(|f| f.is_srgb()).copied().unwrap_or(surface_caps.formats[0]); 
+        let size = window.inner_size();
+    SurfaceConfiguration { usage: TextureUsages::RENDER_ATTACHMENT, format: surface_format, width: size.width, height: size.height, present_mode: surface_caps.present_modes[0], desired_maximum_frame_latency: 2, alpha_mode: surface_caps.alpha_modes[0], view_formats: vec![] }
     }
 }
 
