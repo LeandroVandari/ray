954e8fd2ad4e41bdfa16949aaaa355edc6f05014
LeandroVandari <bernardokreutz@gmail.com>
2025-03-25 22:59:16 -0300

    Creating adapter, device and queue properly

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::sync::Arc;
 
-use wgpu::{Backends, CreateSurfaceError, Instance, InstanceDescriptor, Surface};
+use pollster::FutureExt as _;
+use wgpu::{
+    Backends, CreateSurfaceError, Device, DeviceDescriptor, Instance, InstanceDescriptor, Queue,
+    RequestAdapterOptions, Surface,
+};
 use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
@@ -16,6 +20,8 @@ struct SurfaceState<'window> {
     window: Arc<Window>,
     instance: Instance,
     surface: Surface<'window>,
+    device: Device,
+    queue: Queue,
 }
 
 impl<'window> SurfaceState<'window> {
@@ -36,10 +42,26 @@ impl<'window> SurfaceState<'window> {
                 .unwrap(),
         );
 
+        let surface = instance.create_surface(window.clone())?;
+
+        let adapter = instance
+            .request_adapter(&RequestAdapterOptions {
+                compatible_surface: Some(&surface),
+                ..Default::default()
+            })
+            .block_on()
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(&DeviceDescriptor::default(), None)
+            .block_on()
+            .unwrap();
+
         Ok(Self {
-            surface: instance.create_surface(window.clone())?,
+            surface,
             instance,
             window,
+            device,
+            queue,
         })
     }
 }
